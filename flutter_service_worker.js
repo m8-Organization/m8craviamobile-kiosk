10d3feceb282b222e710f4c6a66c871f4e3afb9b
sreya-999 <sreya.m@m8itsolutions.com>
2026-01-09 11:26:33 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -415,7 +415,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c7ec422cd8f6bc5553e4cda4e3ba6fe2
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "eb6344809c60a2b3a78da119bddfd3c8",
+"flutter_bootstrap.js": "b822d1e606157620ab813eb77bbb992a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
